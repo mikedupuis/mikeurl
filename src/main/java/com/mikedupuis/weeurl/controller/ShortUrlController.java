8cdc0753c2f01b66d0c6facd644ab27ee1abc0a4
Michael.Dupuis <michael.dupuis@target.com>
2019-08-26 12:36:11 -0500

    Remove unused import

--- src/main/java/com/mikedupuis/weeurl/controller/ShortUrlController.java
+++ src/main/java/com/mikedupuis/weeurl/controller/ShortUrlController.java
@@ -3,7 +3,6 @@ package com.mikedupuis.weeurl.controller;
 import com.mikedupuis.weeurl.dto.WeeurlCreateRequest;
 import com.mikedupuis.weeurl.entity.WeeUrl;
 import com.mikedupuis.weeurl.service.WeeUrlService;
-import com.sun.javaws.exceptions.InvalidArgumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
